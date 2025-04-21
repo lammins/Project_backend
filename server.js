94f217fe998ec8566a8ac42035eed4fc7fb07d31
lammins <142163910+lammins@users.noreply.github.com>
2025-04-21 21:54:24 +0700

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const PORT = process.env.PORT || 5000;
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: 'https://projectbackend-copy-production.up.railway.app', credentials: true }));
+
 app.use(express.json({ limit: '20mb' }));
 
 mongoose.connect(process.env.MONGO_URL)
